ac2ff6da04d338a4395f6cabbd0315d9a130b109
Herman Slatman <hermanslatman@hotmail.com>
2023-01-19 22:59:30 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/hslatman/step-template-playground
+
+go 1.19
+
+require (
+	filippo.io/edwards25519 v1.0.0 // indirect
+	github.com/Masterminds/goutils v1.1.1 // indirect
+	github.com/Masterminds/semver/v3 v3.1.1 // indirect
+	github.com/Masterminds/sprig/v3 v3.2.2 // indirect
+	github.com/go-logr/logr v1.2.0 // indirect
+	github.com/google/certificate-transparency-go v1.1.4 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/huandu/xstrings v1.3.2 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/shopspring/decimal v1.2.0 // indirect
+	github.com/smallstep/certinfo v1.10.0 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	go.step.sm/crypto v0.23.1 // indirect
+	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
+	golang.org/x/net v0.1.0 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	k8s.io/klog/v2 v2.80.1 // indirect
+)
---
+++ template.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"errors"
+	"fmt"
+
+	"go.step.sm/crypto/keyutil"
+	"go.step.sm/crypto/minica"
+
+	"github.com/smallstep/certinfo"
+)
+
+func Execute(template string, commonName string) (*x509.Certificate, error) {
+
+	if template == "" {
+		return nil, errors.New("template can not be empty")
+	}
+
+	ca, err := minica.New(
+		minica.WithName("Go Playground CA (powered by minica)"),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := keyutil.GenerateDefaultSigner()
+	if err != nil {
+		return nil, err
+	}
+
+	cr := &x509.CertificateRequest{
+		Subject: pkix.Name{
+			CommonName: commonName,
+		},
+	}
+
+	csr, err := x509.CreateCertificateRequest(rand.Reader, cr, s)
+	if err != nil {
+		return nil, err
+	}
+
+	cr, err = x509.ParseCertificateRequest(csr)
+	if err != nil {
+		return nil, err
+	}
+
+	opts := []minica.SignOption{
+		minica.WithTemplate(template),
+	}
+
+	c, err := ca.SignCSR(cr, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func Print(c *x509.Certificate) error {
+	txt, err := certinfo.CertificateText(c)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(txt)
+
+	return nil
+}
